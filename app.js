a52280ee6772d111c64baa2a7f06f66dfc9a597b
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2023-01-27 01:08:28 +0200

    Adding new list items to the to do list.

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy food","Clean House","Take a shower"];
+
 app.set('view engine','ejs');
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/", function(req, res){
+
   var today = new Date();
 
   var options = {
@@ -16,9 +21,13 @@ app.get("/", function(req, res){
 
   var day = today.toLocaleDateString("en-US",options);
 
-  res.render("list",{
-    kindOfDay: day
-  });
+  res.render("list",{kindOfDay: day,newListItems:items});
+});
+
+app.post("/",function(req,res){
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, function(){
