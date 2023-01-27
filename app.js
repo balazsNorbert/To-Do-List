9aa7e67496753192aadf9a10fd84968d5f938232
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2023-01-27 11:36:27 +0200

    Adding header and footer with ejs templates.Get and Post request for work page.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy food","Clean House","Take a shower"];
-
+let workItems =[]
 app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -22,13 +22,31 @@ app.get("/", function(req, res){
 
   let day = today.toLocaleDateString("en-US",options);
 
-  res.render("list",{kindOfDay: day,newListItems:items});
+  res.render("list",{listTitle: day,newListItems:items});
 });
 
 app.post("/",function(req,res){
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  if(req.body.list === "work"){
+    workItems.push(item);
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work",function(req,res){
+  res.render("list",{listTitle: "Work List",newListItems: workItems});
+});
+
+app.post("/work",function(req,res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
+
+app.get("/about",function(req,res){
+  res.render("about");
 });
 
 app.listen(3000, function(){
