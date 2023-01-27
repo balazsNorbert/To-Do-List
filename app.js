d2e558e4f101603f51c0a2ddac49cc53d1c3b904
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2023-01-27 11:51:25 +0200

    Separate date in a function in a different file.

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy food","Clean House","Take a shower"];
-let workItems =[]
+const items = ["Buy food","Clean House","Take a shower"];
+const workItems =[]
 app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US",options);
+  const day = date.getDate();
 
   res.render("list",{listTitle: day,newListItems:items});
 });
 
 app.post("/",function(req,res){
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   if(req.body.list === "work"){
     workItems.push(item);
   }else{
@@ -40,7 +32,7 @@ app.get("/work",function(req,res){
 });
 
 app.post("/work",function(req,res){
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
 })
