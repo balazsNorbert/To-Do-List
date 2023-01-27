d997d7d89ee68d32aff31aa1fcd711c015bf595f
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2023-01-28 01:18:44 +0200

    Create custom lists.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -13,11 +12,11 @@ app.use(express.static("public"));
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb://localhost:27017/todoDB",{family: 4});
 
-const itemSchema = {
+const itemsSchema = {
   name: String
 };
 
-const Item = mongoose.model("Item",itemSchema);
+const Item = mongoose.model("Item",itemsSchema);
 
 const item1 = new Item ({
   name: "Welcome to your todolist!"
@@ -33,10 +32,14 @@ const item3 = new Item ({
 
 const defaultItems = [item1,item2,item3];
 
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List",listSchema);
 
 app.get("/", function(req, res){
-  const day = date.getDate();
-
   Item.find({},function(err,foundItems){
     if(foundItems.length === 0){
       Item.insertMany(defaultItems,function(err){
@@ -48,18 +51,27 @@ app.get("/", function(req, res){
     });
     res.redirect("/");
   }else{
-      res.render("list",{listTitle: day,newListItems:foundItems});
+      res.render("list",{listTitle: "Today",newListItems:foundItems});
   }
   });
 });
 
 app.post("/",function(req,res){
   const itemName = req.body.newItem;
-  item = new Item({
+  const listName = req.body.list;
+  const item = new Item({
     name: itemName
   });
-  item.save();
-  res.redirect("/");
+  if(listName === "Today"){
+    item.save();
+    res.redirect("/");
+  }else{
+    List.findOne({name: listName},function(err,foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    })
+  }
 });
 
 app.post("/delete",function(req,res){
@@ -72,8 +84,23 @@ app.post("/delete",function(req,res){
   });
 });
 
-app.get("/work",function(req,res){
-  res.render("list",{listTitle: "Work List",newListItems: workItems});
+app.get("/:customListName",function(req,res){
+  const customListName = req.params.customListName;
+  List.findOne({name:customListName},function(err,foundList){
+    if(!err){
+      if(!foundList){
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        })
+        list.save();
+        res.redirect("/" + customListName);
+      }else{
+        res.render("list",{listTitle:foundList.name,newListItems:foundList.items});
+      }
+    }
+  })
+
 });
 
 app.post("/work",function(req,res){
